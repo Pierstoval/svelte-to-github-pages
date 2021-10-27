95acf480da6a6370360c2ea40b139bd4bfd6b290
Alex Rock Ancelet <pierstoval@gmail.com>
2021-10-27 14:32:14 +0200

    Init

---
+++ src/routes/index.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    let count = 0;
+
+    function incrementCounter() {
+        count++;
+    }
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+
+<p>Counter: {count}</p>
+
+<button on:click={incrementCounter}>Increment counter</button>
